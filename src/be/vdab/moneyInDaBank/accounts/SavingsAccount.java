79d5d8759d4f4d04d1362d0300241da73a5737a7
Thomas Goos <goosthomas@gmail.com>
2020-07-03 16:10:17 +0200

    SavingsAccount completed with TransferMoney method

--- src/be/vdab/moneyInDaBank/accounts/SavingsAccount.java
+++ src/be/vdab/moneyInDaBank/accounts/SavingsAccount.java
@@ -1,4 +1,38 @@
 package be.vdab.moneyInDaBank.accounts;
 
-public class SavingsAccount {
+public class SavingsAccount extends Account {
+
+    private CurrentAccount currentAccount;
+    private double savingsMoney;
+
+    public SavingsAccount(CurrentAccount currentAccount, double savingsMoney) {
+        this.currentAccount = currentAccount;
+        this.savingsMoney = savingsMoney;
+    }
+
+    public void transferMoney(Account accountfrom, Account accountTo, double amount) {
+        if (accountfrom instanceof CurrentAccount) {
+            this.currentAccount.setCurrentMoney(this.currentAccount.getCurrentMoney() - amount);
+            this.setSavingsMoney(this.getSavingsMoney() + amount);
+        } else {
+            this.currentAccount.setCurrentMoney(this.currentAccount.getCurrentMoney() + amount);
+            this.setSavingsMoney(this.getSavingsMoney() - amount);
+        }
+    }
+
+    public CurrentAccount getCurrentAccount() {
+        return currentAccount;
+    }
+
+    public void setCurrentAccount(CurrentAccount currentAccount) {
+        this.currentAccount = currentAccount;
+    }
+
+    public double getSavingsMoney() {
+        return savingsMoney;
+    }
+
+    public void setSavingsMoney(double savingsMoney) {
+        this.savingsMoney = savingsMoney;
+    }
 }
